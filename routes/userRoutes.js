28123807aaa58af4729081c0cafca6e44cef2670
Macintosh HD <macintoshhd@Macintoshs-MacBook-Pro.local>
2023-08-16 13:49:49 +0100

    fixed likepost functionality

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,9 +15,11 @@ const {
 router.route('/').get(authenticateUser, getAllUsers);
 router.route('/updateUser').patch(authenticateUser, updateUser);
 router.route('/updateUserPassword').patch(authenticateUser, updateUserPassword);
+router.route('/unfollow/:id').put(authenticateUser,unfollowUser);
+
+
 
 router.route('/:id').get(authenticateUser, getSingleUser).delete(authenticateUser,deleteUser).put(authenticateUser,followUser);
-router.route('/:id/unfollow').put(authenticateUser,unfollowUser);
 
 module.exports = router;
 
